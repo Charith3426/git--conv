3d5c3956ab676599e80f3956b550dc0d5255b842
Your Namecharith.jk1856@srmist.edu.in <you@example.com>
2023-07-27 17:24:44 +0530

    codes in java

---
+++ codes.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class MonthToDaysConverter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the month: ");
+        String monthName = scanner.nextLine();
+        scanner.close();
+
+        int numberOfDays = getNumberOfDays(monthName);
+
+        if (numberOfDays != -1) {
+            System.out.println("Number of days in " + monthName + " is: " + numberOfDays);
+        } else {
+            System.out.println("Invalid month name. Please enter a valid month name.");
+        }
+    }
+
+    private static int getNumberOfDays(String monthName) {
+        switch (monthName.toLowerCase()) {
+            case "january":
+            case "march":
+            case "may":
+            case "july":
+            case "august":
+            case "october":
+            case "december":
+                return 31;
+            case "april":
+            case "june":
+            case "september":
+            case "november":
+                return 30;
+            case "february":
+                return 28; // For simplicity, not considering leap years in this example
+            default:
+                return -1; // Invalid month name
+        }
+    }
+}
---
+++ codes2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your password: ");
+        String password = scanner.nextLine();
+        scanner.close();
+
+        if (isValidPassword(password)) {
+            System.out.println("Valid Password");
+        } else {
+            System.out.println("Not a Valid Password");
+        }
+    }
+
+    private static boolean isValidPassword(String password) {
+        // Minimum and maximum length criteria
+        if (password.length() < 6 || password.length() > 16) {
+            return false;
+        }
+
+        // At least 1 letter between [a-z] and 1 letter between [A-Z]
+        if (!password.matches(".*[a-z].*") || !password.matches(".*[A-Z].*")) {
+            return false;
+        }
+
+        // At least 1 number between [0-9]
+        if (!password.matches(".*\\d.*")) {
+            return false;
+        }
+
+        // At least 1 character from [$#@]
+        Pattern specialCharPattern = Pattern.compile("[$#@]");
+        Matcher matcher = specialCharPattern.matcher(password);
+        if (!matcher.find()) {
+            return false;
+        }
+
+        // All criteria met, the password is valid
+        return true;
+    }
+}
